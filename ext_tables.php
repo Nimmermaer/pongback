29659c54ec8804359eb5468edab61a296144487c
Michael B <michidrau@gmail.com>
2023-01-07 23:10:15 +0100

    [TASK] Remove typo3_mode condition

--- ext_tables.php
+++ ext_tables.php
@@ -1,45 +1,40 @@
 <?php
-if (!defined('TYPO3_MODE')) {
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use PHTH\Pongback\Controller\PingbackController;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+if (!defined('TYPO3')) {
     die ('Access denied.');
 }
 
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    $_EXTKEY,
+ExtensionUtility::registerPlugin(
+    'pongback',
     'Pongbackfrontend',
     'LLL:EXT:pongback/Resources/Private/Language/locallang_be.xlf:tx_pongback_plugin.pongback_frontend'
 );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    $_EXTKEY,
+ExtensionUtility::registerPlugin(
+    'pongback',
     'Server',
     'LLL:EXT:pongback/Resources/Private/Language/locallang_be.xlf:tx_pongback_plugin.pongback_server'
 );
 
-if (TYPO3_MODE === 'BE') {
 
     /**
      * Registers a Backend Module
      */
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'PHTH.' . $_EXTKEY,
+    ExtensionUtility::registerModule(
+        'Pongback',
         'web',     // Make module a submodule of 'web'
         'pongbackbackend',    // Submodule key
         '',                        // Position
-        array(
-            'Pingback' => 'administer, show, delete, edit, publish, unpublish ',
-        ),
-        array(
-            'access' => 'user,group',
-            'icon' => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
-            'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_pongbackbackend.xlf',
-        )
+        [PingbackController::class => 'administer, show, delete, edit, publish, unpublish '],
+        ['access' => 'user,group', 'icon' => 'EXT:pongback/ext_icon.gif', 'labels' => 'LLL:EXT:pongback/Resources/Private/Language/locallang_pongbackbackend.xlf']
     );
 
-}
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Pongback');
+ExtensionManagementUtility::addStaticFile('pongback', 'Configuration/TypoScript', 'Pongback');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_pongback_domain_model_pingback',
+ExtensionManagementUtility::addLLrefForTCAdescr('tx_pongback_domain_model_pingback',
     'EXT:pongback/Resources/Private/Language/locallang_csh_tx_pongback_domain_model_pingback.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_pongback_domain_model_pingback');
+ExtensionManagementUtility::allowTableOnStandardPages('tx_pongback_domain_model_pingback');
