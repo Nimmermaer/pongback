d5c34caed1af33fb8ae50322ca6dafaa739947b0
Michael B <michidrau@gmail.com>
2023-01-25 00:10:12 +0100

    [TASK] Respect site config for x-pingback url generation

--- Classes/Middlewares/PingbackHeader.php
+++ Classes/Middlewares/PingbackHeader.php
@@ -10,6 +10,7 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
+use TYPO3\CMS\Core\Site\Entity\Site;
 
 class PingbackHeader implements MiddlewareInterface, LoggerAwareInterface
 {
@@ -17,19 +18,19 @@ class PingbackHeader implements MiddlewareInterface, LoggerAwareInterface
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        /** @var Site $site */
+        $site = $request->getAttribute('site');
         $response = $handler->handle($request);
-        $siteUrl = '';
+
         try {
-            $siteUrl = $request->getAttributes()['normalizedParams']->getSiteUrl();
+            $uri = $site->getRouter()->generateUri($site->getRootPageId(), ['type' => '1392814100']);
+            return $response->withHeader('X-Pingback', $uri->__toString());
         } catch (\Exception) {
             $this->logger->notice('site url was not found', [
-                'url' => $siteUrl,
+                'url' => $uri->__toString(),
             ]);
         }
 
-        if ($siteUrl !== '') {
-            return $response->withHeader('X-Pingback', (string) $siteUrl . '?type=1392814100');
-        }
         return $response;
     }
 }
